758b2f06595b6be2946dc416df1f8cc9682c8469
medamawa <sogonishi@gmail.com>
2020-10-24 22:43:53 +0900

    draftのviewの改善

--- backend/resources/views/draft/index.blade.php
+++ backend/resources/views/draft/index.blade.php
@@ -2,7 +2,7 @@
     <h1>{{ $name }} drafts</h1>
     <ul>
         @foreach ($drafts as $draft)
-            <li>{{ "[" . $draft->title . "] " . $draft->body }}</li>
+            <li>{{ "[" . $draft->title . "] " }} <a href="{{ route('draft.show', ['id' => $draft->id]) }}">詳細を表示</a></li>
             <p>記事ID：<small>{{ $draft->id }}</small></p>
         @endforeach
     </ul>
--- backend/resources/views/draft/show.blade.php
+++ backend/resources/views/draft/show.blade.php
@@ -2,11 +2,13 @@
     <h1>指定した drafts</h1>
     <ul>
         @foreach ($drafts as $draft)
-            <li>{{ "[" . $draft->title . "] " . $draft->body }}</li>
+            <li>{{ "[" . $draft->title . "] " }}</li>
             <p>記事ID：<small>{{ $draft->id }}</small></p>
+            <article>{{!! $draft->body !!}}</article>
+            <br>
             <form method="POST" action="{{ route('draft.submit', ['id' => $draft->id]) }}">
                 @csrf
-                <input type="submit">
+                <input type="submit" value="チェックを依頼をする">
             </form>
         @endforeach
     </ul>
